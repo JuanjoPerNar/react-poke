920cf84a3f1d08c192f66da3a33dde89bda5325a
Guanjino <jjpereiranaranjo@gmail.com>
2025-03-12 11:25:46 +0100

    challenge terminado

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,34 @@ import PokemonCard from "./components/PokemonCard";
 import ErrorMessage from "./components/ErrorMessage";
 import styles from "./App.module.css";
 
+const fetchPokemon = async (searchPok, setPokemon, setError, setLoading) => {
+  if (searchPok === "") {
+    setPokemon(null)
+    setError(null)
+    setLoading(false)
+    return
+  }
+
+  setLoading(true)
+  try {
+    setError(null)
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${searchPok}`)
+    if (!response.ok) {
+      throw new Error("Pokémon no encontrado")
+    }
+    const data = await response.json()
+    setPokemon({
+      name: data.name,
+      image: data.sprites.front_default,
+    })
+  } catch (error) {
+    setPokemon(null)
+    setError(error.message)
+  } finally {
+    setLoading(false)
+  }
+}
+
 function App() {
   const [searchPok, setSearchPok] = useState("")
   const [pokemon, setPokemon] = useState(null)
@@ -15,37 +43,7 @@ function App() {
   }
 
   useEffect(() => {
-    if (searchPok === "") {
-      setPokemon(null)
-      setError(null)
-      setLoading(false)
-      return;
-    }
-
-    const fetchPokemon = async () => {
-      setLoading(true)
-      try {
-        setError(null)
-        const response = await fetch(
-          `https://pokeapi.co/api/v2/pokemon/${searchPok}`
-        );
-        if (!response.ok) {
-          throw new Error("Pokémon no encontrado");
-        }
-        const data = await response.json();
-        setPokemon({
-          name: data.name,
-          image: data.sprites.front_default,
-        });
-      } catch (error) {
-        setPokemon(null)
-        setError(error.message)
-      } finally {
-        setLoading(false)
-      }
-    };
-
-    fetchPokemon()
+    fetchPokemon(searchPok, setPokemon, setError, setLoading)
   }, [searchPok])
 
   return (
